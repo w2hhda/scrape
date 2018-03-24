170e182a9396b10b06e57dbae0a821268de288d2
小闲 <xiaoxian@xiaoxiandeMacBook-Pro.local>
2018-03-24 14:49:16 +0800

    add autopack.sh

---
+++ autopack.sh
@@ -0,0 +1,22 @@
+
+dir=$(pwd)
+
+git pull
+
+bee pack
+
+mkdir ~/scrape
+
+cd ~/scrape
+
+rm -rf *
+
+cd $dir
+
+mv scrape.tar.gz ~/scrape
+
+cd ~/scrape
+
+tar zxvf scrape.tar.gz
+
+nohub ./scrape &
